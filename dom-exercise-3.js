7f6683b2d07babc566e6b5e71b1494b60e0fbd12
Ankur <vasavada.ankur@gmail.com>
2020-03-11 21:53:26 -0400

    stuff

--- dom-exercise-3.js
+++ dom-exercise-3.js
@@ -1,26 +1,40 @@
-document.onmousemove = function(e) {
-	// create variable for the circle
-	// // for trails
-	let circle = document.createElement('div');
-	// let circle = document.queryCommandValue('.circle');
+const circle = document.querySelector('.circle');
 
-	// set x and y variables
-	// clientX/Y gets the horizontal & vertical coordinates
-	let x = e.clientX;
-	let y = e.clientY;
+document.addEventListener('mousemove', e => {
+	circle.setAttribute('style', 'top: ' + (e.pageY - 25) + 'px; left: ' + (e.pageX - 25) + 'px;');
+});
 
-	// style.left/top sets the position of an object from the left or top of a positioned div
-	// the style attribute needs px to function, which is why we concatenate the 'px'
-	circle.style.left = x + 'px';
-	circle.style.top = y + 'px';
+// document.addEventListener('click', () => {
+// 	circle.classList.add('expand');
+// 	setTimeout(() => {
+// 		circle.classList.remove('expand');
+// 	}, 300);
+// });
 
-	// // ***comment out everything below this to remove trails
-	// // set the class attribute for the div we created to circle
-	circle.setAttribute('class', 'circle');
-	// add the circle div to the page
-	document.body.appendChild(circle);
-	// // but remove it every .25 secs
-	setTimeout(function() {
-		document.body.removeChild(circle);
-	}, 250);
-};
+// // for trails
+// document.onmousemove = function(e) {
+// 	// create variable for the circle
+// 	// // for trails
+// 	let circle = document.createElement('div');
+// 	// let circle = document.queryCommandValue('.circle');
+
+// 	// set x and y variables
+// 	// clientX/Y gets the horizontal & vertical coordinates
+// 	let x = e.clientX;
+// 	let y = e.clientY;
+
+// 	// style.left/top sets the position of an object from the left or top of a positioned div
+// 	// the style attribute needs px to function, which is why we concatenate the 'px'
+// 	circle.style.left = x + 'px';
+// 	circle.style.top = y + 'px';
+
+// 	// // ***comment out everything below this to remove trails
+// 	// // set the class attribute for the div we created to circle
+// 	circle.setAttribute('class', 'circle');
+// 	// add the circle div to the page
+// 	document.body.appendChild(circle);
+// 	// // but remove it every .25 secs
+// 	setTimeout(function() {
+// 		document.body.removeChild(circle);
+// 	}, 250);
+// };
